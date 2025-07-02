422f10c050139c1e1d3bf5dffb68ea7ac256e962
Magdalena Bober <magdakochman7@gmail.com>
2025-07-02 16:05:47 +0200

    feat: setup app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CodeSnippetComponent } from './features/code-snippet/code-snippet.component';
+import { ShowcaseCardComponent } from './features/showcase-card/showcase-card.component';
+import { TextInputComponent } from "./features/text-input/text-input.component";
+import { FormsModule } from '@angular/forms';
+import { DegreePickerComponent } from "./features/degree-picker/degree-picker.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'gg-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [CodeSnippetComponent, ShowcaseCardComponent, TextInputComponent, FormsModule, DegreePickerComponent],
 })
 export class App {
-  protected title = 'gradient-generator';
+  text = ''
 }
