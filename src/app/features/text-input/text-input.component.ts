0cc39595a720d4880a303262f67872a8ef71ca85
Magdalena Bober <magdakochman7@gmail.com>
2025-07-02 16:04:25 +0200

    feat: add text-input component

---
+++ src/app/features/text-input/text-input.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'gg-text-input',
+  template: `<textarea class="gg-text-input" [(ngModel)]="value" (ngModelChange)="valueChange.emit(value)"></textarea>`,
+  styleUrl: './text-input.component.scss',
+  imports: [CommonModule, FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
+})
+export class TextInputComponent implements OnInit {
+  valueChange = output<string>();
+  value: string = 'I\'m just a girl';
+
+  ngOnInit(): void {
+    this.valueChange.emit(this.value);
+  }
+}
