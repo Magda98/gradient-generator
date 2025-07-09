915be8dee9868c9a6c742b4a6c2624878bceb2e2
Magdalena Bober <magdakochman7@gmail.com>
2025-07-09 18:08:19 +0200

    feat: update components to connect co color list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, model, signal } from '@angular/core';
 import { CodeSnippetComponent } from './features/code-snippet/code-snippet.component';
 import { ShowcaseCardComponent } from './features/showcase-card/showcase-card.component';
 import { TextInputComponent } from "./features/text-input/text-input.component";
 import { FormsModule } from '@angular/forms';
 import { DegreePickerComponent } from "./features/degree-picker/degree-picker.component";
+import { ColorListComponent } from "./features/color-list/color-list.component";
 
 @Component({
   selector: 'gg-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [CodeSnippetComponent, ShowcaseCardComponent, TextInputComponent, FormsModule, DegreePickerComponent],
+  imports: [CodeSnippetComponent, ShowcaseCardComponent, TextInputComponent, FormsModule, DegreePickerComponent, ColorListComponent],
 })
 export class App {
+  colors = signal<Record<string, string>>({
+    color1: '#9d39e5',
+    color2: '#142243',
+    color3: '#dc454b'
+  });
   text = ''
+  angle = 135;
+  colorsValues = computed(() => {
+    return Object.values(this.colors());
+  });
 }
--- src/app/features/showcase-card/showcase-card.component.ts
+++ src/app/features/showcase-card/showcase-card.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gg-showcase-card',
   templateUrl: './showcase-card.component.html',
   styleUrls: ['./showcase-card.component.scss'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule]
 })
 export class ShowcaseCardComponent {
   text = input<string>('showcase-card works!');
+  colors = input<Array<string>>([]);
+  angle = input<number>(135);
+  background = computed(() => {
+    return `background: linear-gradient(${this.angle()}deg, ${this.colors().join(', ')});`;
+  });
 }
