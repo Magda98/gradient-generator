bc55aa6f68296770298ede8bebc4402262ee3e3a
Magdalena Bober <magdakochman7@gmail.com>
2025-07-09 18:07:54 +0200

    feat: add color picker component

---
+++ src/app/features/color-picker/color-picker.component.ts
@@ -0,0 +1,58 @@
+import { ChangeDetectionStrategy, Component, computed, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'gg-color-picker',
+  imports: [FormsModule],
+  template: `
+  <div>
+  <button class="gg-color-picker__trigger" [style]="'anchor-name: --' + anchorName()" [attr.popovertarget]="anchorName()" [style.background-color]="color()" [style.color]="textColor()">
+    {{color()}}
+  </button>
+  <div [attr.id]="anchorName()" popover class="tooltip" [style]="
+    'position-anchor: --' + anchorName() + ';'
+  ">
+  <input class="gg-color-picker__input" type="text" [(ngModel)]="color" />
+  </div>
+</div>
+`,
+  styleUrl: './color-picker.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ColorPickerComponent {
+  color = model('#9d39e5')
+  protected anchorName = signal<string>(`tooltip-${crypto.randomUUID()}`)
+
+  textColor = computed( () => {
+    // remove hash
+    let hexcolor = this.color().replace("#", "");
+
+    // handle hex shorthand (e.g., "03F"), expand it
+    if (hexcolor.length === 3) {
+      hexcolor = hexcolor.split('').map(function (hex) {
+        return hex + hex;
+      }).join('');
+    }
+
+
+    const r = parseInt(hexcolor.substring(0, 2), 16);
+    const g = parseInt(hexcolor.substring(2, 4), 16);
+    const b = parseInt(hexcolor.substring(4, 6), 16);
+
+    // calculate luminance
+    const getLum = (c: number) => {
+      const sRGB = c / 255;
+      return sRGB <= 0.03928 ? sRGB / 12.92 : Math.pow((sRGB + 0.055) / 1.055, 2.4);
+    };
+
+    const backgroundLum = 0.2126 * getLum(r) + 0.7152 * getLum(g) + 0.0722 * getLum(b);
+
+    // calculate contrast ratio
+    const contrastWithWhite = (1 + 0.05) / (backgroundLum + 0.05);
+    const contrastWithBlack = (backgroundLum + 0.05) / (0 + 0.05);
+
+    return contrastWithWhite > contrastWithBlack ? '#fff' : '#000';
+  })
+
+
+}
