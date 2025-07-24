19421872e027aeeddb760c312c222585867808f4
Magdalena Bober <magdakochman7@gmail.com>
2025-07-24 17:58:52 +0200

    feat: add drag&drop to color list

--- src/app/features/color-list/color-list.component.ts
+++ src/app/features/color-list/color-list.component.ts
@@ -1,23 +1,29 @@
 import { ChangeDetectionStrategy, Component, computed, model } from '@angular/core';
 import { ColorPickerComponent } from "../color-picker/color-picker.component";
 import { TrashIconComponent } from "../../shared/components/trash-icon.component";
+import { CdkDrag, CdkDragDrop, CdkDragPlaceholder, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
+import { DragIconComponent } from "../../shared/components/drag-icon.component";
 
 @Component({
   selector: 'gg-color-list',
-  imports: [ColorPickerComponent, TrashIconComponent],
+  imports: [ColorPickerComponent, TrashIconComponent, CdkDropList, CdkDrag, DragIconComponent],
   template: `
   <div class="gg-color-list-header">
     <button class="gg-button" (click)="addColor()">Add Color</button>
   </div>
+  <div cdkDropList  (cdkDropListDropped)="drop($event)">
   @for(item of colorsEntries(); track item[0]) {
-    <div class="gg-color-item">
+    <div cdkDrag class="gg-color-item">
+    <button class="gg-button gg-button--icon gg-button--drag" (click)="removeColor(item[0])">
+      <gg-drag-icon></gg-drag-icon>
+      </button>
       <gg-color-picker [color]="item[1]" (colorChange)="colorChange(item[0], $event)"></gg-color-picker>
       <button class="gg-button gg-button--icon" (click)="removeColor(item[0])">
       <gg-trash-icon></gg-trash-icon>
       </button>
     </div>
-  }
-
+    }
+  </div>
   `,
   styleUrl: './color-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -33,6 +39,12 @@ export class ColorListComponent {
     const newColors = { ...this.colors() };
     newColors[colorId] = $event;
     this.colors.set(newColors);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    const colors = this.colorsEntries()
+    moveItemInArray(colors, event.previousIndex, event.currentIndex);
+    this.colors.set(Object.fromEntries(colors));
   }
 
   removeColor(colorId: string) {
